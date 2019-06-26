7917cfe38cd8994b9749b6109b47622fd17ded26
Dell_MVMS <companymvms@gmail.com>
2019-06-26 10:12:18 +0200

    add: number guesser + js

---
+++ Abschnitt4-40-NumberGuesser-Part02/app.js
@@ -0,0 +1,60 @@
+/*
+GAME FUNCTION:
+- Player must guess an umber between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
+
+/*Note: in the console output (Devtools) numbers are colored blue and string are colored black*/
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+//UI Elements
+const game = document.querySelector("#game"),
+      minNum = document.querySelector(".min-num"),
+      maxNum = document.querySelector(".max-num"),
+      guessBtn = document.querySelector("#guess-btn"),
+      guessInput = document.querySelector("#guess-input"),
+      message = document.querySelector(".message");
+
+// Assign UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener("click", function() {
+
+  let guess = parseInt(guessInput.value);
+  
+  //Validate: NaN: not a number
+  if(isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  // Check if won
+  if(guess === winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = "green";
+    // Set message
+    setMessage(`${winningNum} is correct! Congratulations! You Win!`, "green");
+    //
+  } else {
+
+  }
+
+});
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
