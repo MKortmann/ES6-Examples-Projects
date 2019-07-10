c7dd269cb1f9d34cda662a38f92deb8c8f8ef360
Dell_MVMS <companymvms@gmail.com>
2019-07-10 11:07:21 +0200

    add: ES6 Promises & The Fetch API

---
+++ Abschnitt6-67-ArrowFunctions/app.js
@@ -0,0 +1,55 @@
+document.getElementById("button1").addEventListener("click", getText);
+document.getElementById("button2").addEventListener("click", getJson);
+document.getElementById("button3").addEventListener("click", getExternal);
+
+// you have to use fetch 2 times because fetch return a promise. And the response.text() returns also a promise.
+// GET LOCAL TEXT FILE
+function getText() {
+  fetch("text.txt")
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+// GET LOCAL JSON DATA
+function getJson() {
+  fetch("posts.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(function(post) {
+        output += `<li>${post.title}</li>`;
+      })
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+
+// GET LOCAL External API
+function getExternal() {
+  fetch("https://api.github.com/users")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(function(user) {
+        output += `<li>${user.login}</li>`;
+      })
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
