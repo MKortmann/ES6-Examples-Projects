51130cb3a691ee1221df05514d6b7071b14c71ea
Dell_MVMS <companymvms@gmail.com>
2019-07-11 17:53:22 +0200

    using async away instead of rapping inside a promise

---
+++ Abschnitt6-70-CustomHTTPLibrary(FetchWithAsync)/easyhttp3.js
@@ -0,0 +1,60 @@
+
+/**
+* EasyHTTP Library: Using Fetch, promises and arrow functions
+* Library for making HTTP requests
+*
+* @version 2.0.0
+* @author XXXX YYYY
+* @license MIT
+*
+*/
+
+class EasyHTTP {
+  //  MAKE an HTTP GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // MAKE an HTTP POST Request
+  async post(url, data) {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+      const resData = await response.json();
+      return resData;
+  }
+
+ // Make an HTTP Put Request
+ async put(url, data) {
+    const response = await fetch(url,{
+       method: "PUT",
+       headers: {
+         "Content-type": "application/json"
+       },
+       body: JSON.stringify(data)
+   })
+   const resData = await response.json();
+   return resData;
+ }
+
+ // Make an HTTP Delete Request
+ async delete(url) {
+   const response = await fetch(url, {
+     method: "DELETE",
+     headers: {
+       "Content-type": "application/json"
+     }
+   })
+   const resData = await "resource delted";
+   return resData;
+
+ }
+
+
+}
